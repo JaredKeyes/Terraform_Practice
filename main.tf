1c441e677917ad6442fa4f43244f116f2f5ce02e
Jared Keyes <jared.keyes3@gmail.com>
2026-05-31 20:18:59 -0400

    Switched to autoscaling group, and added variable for port declaration.

--- main.tf
+++ main.tf
@@ -1,12 +1,50 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-091138d0f0d41ff90"
-  instance_type = "t2.micro"
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
 
-  tags = {
-    Name = "terraform-example"
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-091138d0f0d41ff90"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuaration.example.name
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
